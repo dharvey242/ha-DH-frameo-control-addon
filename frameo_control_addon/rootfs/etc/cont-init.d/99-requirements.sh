8ba76e142d783ba0844463fc600123eca84e8136
Hunor Laczko <luni741@gmail.com>
2025-06-22 23:14:39 +0200

    fix pip install problem

--- frameo_control_addon/rootfs/etc/cont-init.d/99-requirements.sh
+++ frameo_control_addon/rootfs/etc/cont-init.d/99-requirements.sh
@@ -3,5 +3,5 @@
 # Install required Python packages
 if bashio::fs.file_exists "/requirements.txt"; then
     bashio::log.info "Installing Python packages from /requirements.txt..."
-    pip install --no-cache-dir -r /requirements.txt
+    pip install --no-cache-dir --break-system-packages -r /requirements.txt
 fi
